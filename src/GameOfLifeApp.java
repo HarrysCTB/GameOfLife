ce809091ec8a8fe440f89b33b93786e32876fe3f
Elarif Coding <71882575+HarrysCTB@users.noreply.github.com>
2024-03-14 17:15:59 +0100

    feature(GameOfLife): Add menu bar with File and Edit sections
    Implemented a menu bar in the GameOfLife application, comprising two main menus: File and Edit. The File menu provides an option to exit the application, while the Edit menu allows for adding a cell, pausing, resuming, and resetting the game. These enhancements aim to improve user interaction and provide convenient controls for managing the game lifecycle.
    
    This makes the application more intuitive and aligns with user expectations for basic functionality in a Game of Life application.

--- src/GameOfLifeApp.java
+++ src/GameOfLifeApp.java
@@ -1,6 +1,9 @@
 package src;
 import javax.swing.JFrame;
 import javax.swing.Timer;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
@@ -17,6 +20,60 @@ public class GameOfLifeApp extends JFrame {
 
         int gridWidth = windowWidth / cellSize;
         int gridHeight = windowHeight / cellSize;
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Fichier");
+        JMenuItem newItem = new JMenuItem("Nouveau");
+        JMenuItem openItem = new JMenuItem("Ouvrir");
+        JMenuItem saveItem = new JMenuItem("Enregistrer");
+        JMenuItem saveAsItem = new JMenuItem("Enregistrer sous");
+        JMenuItem exitItem = new JMenuItem("Quitter");
+        exitItem.addActionListener(e -> System.exit(0));
+        fileMenu.add(newItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+        JMenu editMenu = new JMenu("Édition");
+        JMenuItem addCellItem = new JMenuItem("Ajouter une cellule");
+        JMenuItem pauseItem = new JMenuItem("Mettre en pause");
+        JMenuItem resumeItem = new JMenuItem("Reprendre");
+        JMenuItem resetItem = new JMenuItem("Réinitialiser");
+
+        pauseItem.addActionListener(e -> timer.stop());
+        resumeItem.addActionListener(e -> timer.start());
+        resetItem.addActionListener(e -> {
+            timer.stop();
+            grid = new CellGrid(gridWidth, gridHeight);
+            panel.updateGrid(grid, windowWidth, windowHeight);
+            timer.start();
+        });
+        addCellItem.addActionListener(e -> {
+            int randomX = (int) (Math.random() * grid.getWidth());
+            int randomY = (int) (Math.random() * grid.getHeight());
+            grid.setAlive(randomX, randomY, true);
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    int neighbourX = randomX + i;
+                    int neighbourY = randomY + j;
+                    if (neighbourX >= 0 && neighbourX < grid.getWidth() && neighbourY >= 0 && neighbourY < grid.getHeight()) {
+                        grid.setAlive(neighbourX, neighbourY, true);
+                    }
+                }
+            }
+            panel.repaint();
+        });
+
+        editMenu.add(addCellItem);
+        editMenu.add(pauseItem);
+        editMenu.add(resumeItem);
+        editMenu.add(resetItem);
+        menuBar.add(editMenu);
+
+        setJMenuBar(menuBar);
 
         grid = new CellGrid(gridWidth, gridHeight);
         panel = new GridPanel(grid, windowWidth, windowHeight);
@@ -46,9 +103,13 @@ public class GameOfLifeApp extends JFrame {
     }
 
     public static void main(String[] args) {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "GameOfLifeApp");
+
         java.awt.EventQueue.invokeLater(() -> {
             new GameOfLifeApp().setVisible(true);
         });
     }
+
 }
 
